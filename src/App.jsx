a36c255fdea4cf57e855a356bb2f88d92bc2c8af
AmilcarGPC <A18003370@alumnos.uady.mx>
2025-11-08 03:01:07 -0600

    Non ocl

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import ThermalHouseSimulator from "./components/ThermalHouseSimulator.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ThermalHouseSimulator />
+    </>
+  );
+}
+
+export default App;
